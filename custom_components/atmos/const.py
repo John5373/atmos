7dfe12189fab53553de8931a12c518dab6fed3c9
John5373 <maniscalcojohn@gmail.com>
2025-03-09 22:14:27 -0500

    Update const.py

--- custom_components/atmos/const.py
+++ custom_components/atmos/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "atmosenergy"
 
-DEFAULT_NAME = "Atmos Energy Usage"
+DEFAULT_NAME = "Atmos Energy Usage v2"
 DEFAULT_SCAN_INTERVAL = 3600  # seconds, i.e. 1 hour
 
 CONF_USERNAME = "username"
