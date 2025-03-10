0934f6ed5382b79cfbfc00a4d9cb3e8e8807b44f
John5373 <maniscalcojohn@gmail.com>
2025-03-09 23:39:19 -0500

    Update config_flow.py

--- custom_components/atmos/config_flow.py
+++ custom_components/atmos/config_flow.py
@@ -4,14 +4,12 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
 
-from . import DOMAIN, validate_credentials
-
 DATA_SCHEMA = vol.Schema({
     vol.Required(CONF_USERNAME): str,
     vol.Required(CONF_PASSWORD): str,
 })
 
-class AtmosEnergyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class AtmosEnergyConfigFlow(config_entries.ConfigFlow, domain="atmosenergy"):
     """Handle a config flow for AtmosEnergy."""
 
     VERSION = 1
@@ -22,6 +20,8 @@ class AtmosEnergyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             username = user_input[CONF_USERNAME]
             password = user_input[CONF_PASSWORD]
+            # Defer the heavy import until needed.
+            from . import validate_credentials
             valid = await self.hass.async_add_executor_job(validate_credentials, username, password)
             if valid:
                 return self.async_create_entry(title="AtmosEnergy", data=user_input)
